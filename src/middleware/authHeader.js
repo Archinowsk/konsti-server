ad86bdd617a954e4ac74863219c2450d391bac42
Arttu Hanska <arttu.hanska@gmail.com>
2017-07-23 22:08:03 +0300

    JWT

--- src/middleware/authHeader.js
+++ src/middleware/authHeader.js
@@ -3,3 +3,9 @@ const logger = require('../utils/logger').logger;
 const config = require('../../config');
 
 const jwtSecretKey = config.jwtSecretKey;
+
+const authHeader = () => {
+  logger.info('Require JWT');
+};
+
+module.exports = authHeader;
