3c29c7fa8ec3f6b8d4ef55403641e512be714a5d
Arttu Hanska <arttu.hanska@gmail.com>
2017-07-24 15:41:59 +0300

    Adjust jwt

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.use(bodyParser.json({ limit: '100kb' }));
 app.use(
   expressJWT({ secret: config.jwtSecretKey }).unless({
     path: [
-      // { url: '/', methods: ['GET'], ext: ['.js', '.css'] },
+      { url: '/', methods: ['GET'] },
       // { ext: ['.js', '.css', '.html'] },
       // { url: '/index.html', methods: ['GET'] },
       { url: '/api/login', methods: ['POST', 'OPTIONS'] },
