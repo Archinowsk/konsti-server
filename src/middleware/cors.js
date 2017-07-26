f2db8d9991e497acdabfd33c14ac19ad98dee99a
Arttu Hanska <arttu.hanska@gmail.com>
2017-07-26 19:12:21 +0300

    CORS - skip undefined

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -5,13 +5,14 @@ function allowCrossDomain(req, res, next) {
   const allowedOrigins = config.allowedCorsOrigins;
   const origin = req.headers.origin;
 
-  if (allowedOrigins.indexOf(origin) > -1) {
-    // logger.info(`CORS: Allow from ${origin}`);
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  } else {
-    logger.info(`CORS: Block from ${origin}`);
+  if (typeof origin !== 'undefined') {
+    if (allowedOrigins.indexOf(origin) > -1) {
+      // logger.info(`CORS: Allow from ${origin}`);
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+      logger.info(`CORS: Block from ${origin}`);
+    }
   }
-
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
