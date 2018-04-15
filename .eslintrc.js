64dfc96058417b2bf39dfc3b2c4b11f230bcd934
Arttu Hanska <arttu.hanska@gmail.com>
2018-04-15 17:30:12 +0300

    Update eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,14 @@
 module.exports = {
-  extends: ['airbnb', 'plugin:react/recommended', 'prettier', 'prettier/react'],
-  // parser: 'babel-eslint',
+  extends: ['standard', 'prettier', 'prettier/standard'],
+  plugins: ['prettier', 'standard'],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2016,
-    ecmaFeatures: {
-      impliedStrict: true,
-      jsx: true,
-    },
+    impliedStrict: true,
   },
-  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
   env: {
-    browser: true,
     node: true,
-    es6: true,
   },
   rules: {
-    'prettier/prettier': ['warn', { trailingComma: 'es5', singleQuote: true }],
-    'no-console': 'off', // Disallow the use of console
-    'linebreak-style': ['warn', 'unix'], // Enforce consistent linebreak style
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: false,
-      },
-    ], // Forbid the use of extraneous packages
-    'no-unused-vars': 'warn', // Disallow Unused Variables
-    'arrow-body-style': [
-      'warn',
-      'as-needed',
-      { requireReturnForObjectLiteral: true },
-    ], // Require braces in arrow function body
-    'react/forbid-prop-types': 'off', // Forbid certain propTypes
+    'prettier/prettier': 'error',
   },
-};
+}
