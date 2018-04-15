42566be4b0a620c4c7f6215af3d6835e9283d4a6
Arttu Hanska <arttu.hanska@gmail.com>
2018-04-15 18:01:03 +0300

    Remove front folder

--- config.js
+++ config.js
@@ -1,39 +1,39 @@
-const config = {};
+const config = {}
 
 // App info
-config.appName = 'Ropecon RPG Signup';
+config.appName = 'Ropecon RPG Signup'
 
 // Server settings
-config.port = process.env.PORT || 3000;
+config.port = process.env.PORT || 3000
 
 // Logging
-config.logDir = './logs';
+config.logDir = './logs'
 
 // Variables for production environment
 if (process.env.NODE_ENV === 'production') {
-  config.env = 'production';
-  config.db = process.env.CUSTOMCONNSTR_CosmosDBConnString;
-  config.jwtSecretKey = process.env.JWT_SECRET_KEY;
-  config.jwtSecretKeyAdmin = process.env.JWT_SECRET_KEY_ADMIN;
-  config.allowedCorsOrigins = ['https://konsti.ropecon.fi'];
+  config.env = 'production'
+  config.db = process.env.CUSTOMCONNSTR_CosmosDBConnString
+  config.jwtSecretKey = process.env.JWT_SECRET_KEY
+  config.jwtSecretKeyAdmin = process.env.JWT_SECRET_KEY_ADMIN
+  config.allowedCorsOrigins = ['https://konsti.ropecon.fi']
 }
 
 // Variables for development environment
 if (process.env.NODE_ENV === 'development') {
-  config.env = 'development';
-  config.db = 'mongodb://localhost/ropecon-rpg-signup';
-  config.jwtSecretKey = 'secret';
-  config.jwtSecretKeyAdmin = 'admin secret';
-  config.allowedCorsOrigins = ['http://localhost:8080'];
+  config.env = 'development'
+  config.db = 'mongodb://localhost/ropecon-rpg-signup'
+  config.jwtSecretKey = 'secret'
+  config.jwtSecretKeyAdmin = 'admin secret'
+  config.allowedCorsOrigins = ['http://localhost:8080']
 }
 
 // Variables for development environment
 if (process.env.NODE_ENV === 'testing') {
-  config.env = 'testing';
-  config.db = 'mongodb://localhost/ropecon-rpg-signup';
-  config.jwtSecretKey = 'secret';
-  config.jwtSecretKeyAdmin = 'admin secret';
-  config.allowedCorsOrigins = [];
+  config.env = 'testing'
+  config.db = 'mongodb://localhost/ropecon-rpg-signup'
+  config.jwtSecretKey = 'secret'
+  config.jwtSecretKeyAdmin = 'admin secret'
+  config.allowedCorsOrigins = []
 }
 
-module.exports = config;
+module.exports = config
