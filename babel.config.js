7887c39d4c57839bc271585295b553d87e57531e
Arttu Hanska <arttu.hanska@gmail.com>
2019-05-03 23:01:29 +0300

    Transpile modules when testing

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 module.exports = api => {
-  api.cache(true)
-
+  api.cache.using(() => process.env.NODE_ENV)
   const presets = [
     [
       '@babel/preset-env',
       {
-        modules: false,
+        modules: api.env('test') ? 'commonjs' : false,
         debug: false,
         targets: {
           node: 'current',
