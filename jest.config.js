5484a81d45152da9588b4d562007985abc425957
Arttu Hanska <arttu.hanska@gmail.com>
2019-05-05 18:12:33 +0300

    Add setupTests file and suppress logging

--- jest.config.js
+++ jest.config.js
@@ -120,7 +120,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ['./src/test/setupTests.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
