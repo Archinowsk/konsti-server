344fab2ad031ffcff4b2b46132004b75b4000462
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-19 16:41:54 +0300

    Add test to verify results collection

--- src/test/testAssignPlayers.js
+++ src/test/testAssignPlayers.js
@@ -77,55 +77,89 @@ const testAssignPlayers = async (): Promise<any> => {
       logger.error(`saveResult error: ${error}`)
     }
 
-    // Verify entered games match signups
-    logger.info('Verify entered games and signups match')
-    let usersAfterAssign = []
-    try {
-      usersAfterAssign = await db.user.findUsers()
-    } catch (error) {
-      logger.error(error)
-    }
-
-    usersAfterAssign.map(user => {
-      const enteredGames = user.enteredGames.filter(
-        enteredGame =>
-          moment(enteredGame.time).format() === moment(startingTime).format()
-      )
-
-      if (!enteredGames || enteredGames.length === 0) return
-
-      if (enteredGames.length !== 1) {
-        logger.error(
-          `Too many entered games for time ${startingTime}: ${user.username} - ${enteredGames.length} games`
-        )
-        return
-      }
-
-      const enteredGame = _.first(enteredGames)
-
-      if (user.signedGames && user.signedGames.length !== 0) {
-        const signupFound = user.signedGames.find(signedGame => {
-          return (
-            signedGame.gameDetails.gameId === enteredGame.gameDetails.gameId &&
-            moment(signedGame.time).format() ===
-              moment(enteredGame.time).format()
-          )
-        })
-
-        if (!signupFound) {
-          logger.error(
-            `Signup not found: ${user.username} - ${enteredGame.gameDetails.title}`
-          )
-        } else {
-          logger.info(
-            `Signup found: ${user.username} - ${enteredGame.gameDetails.title}`
-          )
-        }
-      }
-    })
+    await verifyUserSignups(startingTime)
+    await verifyResults(startingTime)
 
     process.exit()
   }
+}
+
+const verifyUserSignups = async (startingTime: string) => {
+  logger.info('Verify entered games and signups match for users')
+
+  let usersAfterAssign = []
+  try {
+    usersAfterAssign = await db.user.findUsers()
+  } catch (error) {
+    logger.error(error)
+  }
+
+  usersAfterAssign.map(user => {
+    const enteredGames = user.enteredGames.filter(
+      enteredGame =>
+        moment(enteredGame.time).format() === moment(startingTime).format()
+    )
+
+    if (!enteredGames || enteredGames.length === 0) return
+
+    if (enteredGames.length !== 1) {
+      logger.error(
+        `Too many entered games for time ${startingTime}: ${user.username} - ${enteredGames.length} games`
+      )
+      return
+    }
+
+    const enteredGame = _.first(enteredGames)
+
+    if (user.signedGames && user.signedGames.length !== 0) {
+      const signupFound = user.signedGames.find(signedGame => {
+        return (
+          signedGame.gameDetails.gameId === enteredGame.gameDetails.gameId &&
+          moment(signedGame.time).format() === moment(enteredGame.time).format()
+        )
+      })
+
+      if (!signupFound) {
+        logger.error(
+          `Signup not found: ${user.username} - ${enteredGame.gameDetails.title}`
+        )
+      } else {
+        logger.info(
+          `Signup found: ${user.username} - ${enteredGame.gameDetails.title}`
+        )
+      }
+    }
+  })
+}
+
+const verifyResults = async (startingTime: string) => {
+  logger.info('Verify results contain valid data')
+
+  let results
+  try {
+    results = await db.results.findResult(startingTime)
+  } catch (error) {
+    logger.error(`Results: ${error}`)
+  }
+
+  if (!results || !results.result) {
+    logger.error('No results found')
+    return
+  }
+
+  results.result.map(result => {
+    if (
+      moment(result.enteredGame.time).format() !== moment(startingTime).format()
+    ) {
+      logger.error(
+        `Invalid time for "${
+          result.enteredGame.gameDetails.title
+        }" - actual: ${moment(
+          result.enteredGame.time
+        ).format()}, expected: ${startingTime}`
+      )
+    }
+  })
 }
 
 testAssignPlayers()
