85b1911d938b2d5346611f55db7389b65d015087
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-23 22:42:53 +0300

    Add timestamps to game schema

--- src/db/game/gameSchema.js
+++ src/db/game/gameSchema.js
@@ -1,30 +1,33 @@
 /* @flow */
 import mongoose from 'mongoose'
 
-const GameSchema = new mongoose.Schema({
-  gameId: String,
-  title: String,
-  description: String,
-  location: String,
-  startTime: Date,
-  mins: Number,
-  tags: Array,
-  genres: Array,
-  styles: Array,
-  language: String,
-  endTime: Date,
-  people: String,
-  minAttendance: Number,
-  maxAttendance: Number,
-  gameSystem: String,
-  englishOk: Boolean,
-  childrenFriendly: Boolean,
-  ageRestricted: Boolean,
-  beginnerFriendly: Boolean,
-  intendedForExperiencedParticipants: Boolean,
-  shortDescription: String,
-  revolvingDoor: Boolean,
-  popularity: { type: Number, default: 0 },
-})
+const GameSchema = new mongoose.Schema(
+  {
+    gameId: String,
+    title: String,
+    description: String,
+    location: String,
+    startTime: Date,
+    mins: Number,
+    tags: Array,
+    genres: Array,
+    styles: Array,
+    language: String,
+    endTime: Date,
+    people: String,
+    minAttendance: Number,
+    maxAttendance: Number,
+    gameSystem: String,
+    englishOk: Boolean,
+    childrenFriendly: Boolean,
+    ageRestricted: Boolean,
+    beginnerFriendly: Boolean,
+    intendedForExperiencedParticipants: Boolean,
+    shortDescription: String,
+    revolvingDoor: Boolean,
+    popularity: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+)
 
 export const Game = mongoose.model('Game', GameSchema)
