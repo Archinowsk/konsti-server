b9c98c13789f6189883aec43d813d6c0df849402
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-23 19:55:22 +0300

    Add code to generate users and signups for existing games

---
+++ src/test/data-generation/generateUsers.js
@@ -0,0 +1,58 @@
+/* @flow */
+import faker from 'faker'
+import { logger } from 'utils/logger'
+import { db } from 'db/mongodb'
+import {
+  createUsers,
+  createAdminUser,
+  createTestUsers,
+  createUsersInGroup,
+} from 'test/data-generation/generators/createUsers'
+import { createSignups } from 'test/data-generation/generators/createSignups'
+import { config } from 'config'
+
+const generateUsers = async (): Promise<any> => {
+  const newUsersCount = 10
+  const numberOfGroups = 3
+  const groupSize = 3
+
+  const { generateSignups } = config
+
+  if (process.env.NODE_ENV === 'production') {
+    logger.error(`Signup creation not allowed in production`)
+    process.exit()
+  }
+
+  try {
+    await db.connectToDb()
+  } catch (error) {
+    logger.error(`db.connectToDb error: ${error}`)
+  }
+
+  try {
+    logger.info(`MongoDB: Clean old user and results data`)
+    await db.user.removeUsers()
+    await db.results.removeResults()
+  } catch (error) {
+    logger.error(`MongoDB: Clean old data error: ${error}`)
+  }
+
+  logger.info(`MongoDB: Generate new signup data`)
+
+  await createAdminUser()
+  await createTestUsers(2)
+  await createUsers(newUsersCount)
+
+  for (let i = 0; i < numberOfGroups; i++) {
+    const randomGroupId = faker.random.number().toString()
+    await createUsersInGroup(groupSize, randomGroupId)
+  }
+
+  if (generateSignups) {
+    await createSignups('group')
+  }
+
+  process.exit()
+}
+
+generateUsers()
