3484167edc39cd5b4515de7e418e1f151aed9f53
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-23 15:00:15 +0300

    Update game popularity based on opa assignment

--- src/game-popularity/utils/updateWithAssign.js
+++ src/game-popularity/utils/updateWithAssign.js
@@ -1,6 +1,8 @@
 /* @flow */
 import moment from 'moment'
 import _ from 'lodash'
+import { db } from 'db/mongodb'
+import { logger } from 'utils/logger'
 import { opaAssignPlayers } from 'player-assignment/opa/opaAssignPlayers'
 import type { User } from 'flow/user.flow'
 import type { Game } from 'flow/game.flow'
@@ -17,6 +19,26 @@ export const updateWithAssign = async (
     results = results.concat(assignmentResult.results)
   })
 
-  console.log(results)
-  console.log(results.length)
+  const signedGames = results.flatMap(result => result.enteredGame.gameDetails)
+
+  const groupedSignups = signedGames.reduce((acc, game) => {
+    acc[game.gameId] = ++acc[game.gameId] || 1
+    return acc
+  }, {})
+
+  try {
+    await Promise.all(
+      games.map(async game => {
+        if (groupedSignups[game.gameId]) {
+          await db.game.updateGamePopularity(
+            game.gameId,
+            groupedSignups[game.gameId]
+          )
+        }
+      })
+    )
+  } catch (error) {
+    logger.error(`updateGamePopularity error: ${error}`)
+    throw new Error('Update game popularity error')
+  }
 }
