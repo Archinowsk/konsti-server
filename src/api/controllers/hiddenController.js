3c8cd4f1debd190e28718f0d5c496d1ce8042331
Arttu Hanska <arttu.hanska@gmail.com>
2019-07-24 18:19:46 +0300

    Remove game from users when it is hidden

--- src/api/controllers/hiddenController.js
+++ src/api/controllers/hiddenController.js
@@ -3,6 +3,7 @@ import { logger } from 'utils/logger'
 import { db } from 'db/mongodb'
 import { validateAuthHeader } from 'utils/authHeader'
 import type { $Request, $Response, Middleware } from 'express'
+import type { Game } from 'flow/game.flow'
 
 // Add hidden data to server settings
 const postHidden: Middleware = async (
@@ -19,20 +20,93 @@ const postHidden: Middleware = async (
     return res.sendStatus(401)
   }
 
+  let settings = null
   try {
-    const response = await db.settings.saveHidden(hiddenData)
-    return res.json({
-      message: 'Update hidden success',
-      status: 'success',
-      hiddenGames: response.hiddenGames,
-    })
+    settings = await db.settings.saveHidden(hiddenData)
   } catch (error) {
+    logger.error(`db.settings.saveHidden error: ${error}`)
     return res.json({
       message: 'Update hidden failure',
       status: 'error',
       error,
     })
   }
+
+  try {
+    removeHiddenGamesFromUsers(settings.hiddenGames)
+  } catch (error) {
+    logger.error(`removeHiddenGamesFromUsers error: ${error}`)
+    return res.json({
+      message: 'Update hidden failure',
+      status: 'error',
+      error,
+    })
+  }
+
+  return res.json({
+    message: 'Update hidden success',
+    status: 'success',
+    hiddenGames: settings.hiddenGames,
+  })
+}
+
+const removeHiddenGamesFromUsers = async (
+  hiddenGames: $ReadOnlyArray<Game>
+): Promise<any> => {
+  logger.info(`Remove hidden games from users`)
+
+  if (!hiddenGames || hiddenGames.length === 0) return
+
+  logger.info(`Found ${hiddenGames.length} hidden games`)
+
+  let users = null
+  try {
+    users = await db.user.findUsers()
+  } catch (error) {
+    logger.error(`findUsers error: ${error}`)
+    return Promise.reject(error)
+  }
+
+  try {
+    await Promise.all(
+      users.map(async user => {
+        const signedGames = user.signedGames.filter(signedGame => {
+          return hiddenGames.find(hiddenGame => {
+            return hiddenGame.gameId !== signedGame.gameDetails.gameId
+          })
+        })
+
+        const enteredGames = user.enteredGames.filter(enteredGame => {
+          return hiddenGames.find(hiddenGame => {
+            return hiddenGame.gameId !== enteredGame.gameDetails.gameId
+          })
+        })
+
+        const favoritedGames = user.favoritedGames.filter(favoritedGame => {
+          return hiddenGames.find(hiddenGame => {
+            return hiddenGame.gameId !== favoritedGame.gameId
+          })
+        })
+
+        if (
+          user.signedGames.length !== signedGames.length ||
+          user.enteredGames.length !== enteredGames.length ||
+          user.favoritedGames.length !== favoritedGames.length
+        ) {
+          await db.user.updateUser({
+            ...user,
+            signedGames,
+            enteredGames,
+            favoritedGames,
+          })
+        }
+      })
+    )
+  } catch (error) {
+    logger.error(`db.user.updateUser error: ${error}`)
+  }
+
+  logger.info(`Hidden games removed from users`)
 }
 
 export { postHidden }
