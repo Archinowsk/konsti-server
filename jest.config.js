72d88589558d9b7d1c41f3f777249aeeaf0780af
Arttu Hanska <arttu.hanska@gmail.com>
2019-11-13 11:33:56 +0200

    Add Jest setupTestsAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -123,7 +123,7 @@ module.exports = {
   setupFiles: ['./src/test/setupTests.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['./src/test/setupTestsAfterEnv.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
