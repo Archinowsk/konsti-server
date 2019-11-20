eb7475953ec0065be602499ed91265ee9545253b
Arttu Hanska <arttu.hanska@gmail.com>
2019-11-20 21:04:08 +0200

    Refactor removeDeletedGamesFromUsers to own file and add test

--- src/db/game/gameService.js
+++ src/db/game/gameService.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import { logger } from 'utils/logger';
 import { GameModel } from 'db/game/gameSchema';
 import { db } from 'db/mongodb';
+import { removeDeletedGamesFromUsers } from 'player-assignment/utils/removeDeletedGamesFromUsers';
 import type { Game } from 'flow/game.flow';
 
 const removeGames = async (): Promise<void> => {
@@ -98,52 +99,6 @@ const removeMovedGamesFromUsers = async (
             ...user,
             signedGames,
             enteredGames,
-          });
-        }
-      })
-    );
-  } catch (error) {
-    logger.error(`db.user.updateUser error: ${error}`);
-    throw new Error('No assign results');
-  }
-};
-
-export const removeDeletedGamesFromUsers = async () => {
-  logger.info('Remove deleted games from users');
-
-  let users = null;
-  try {
-    users = await db.user.findUsers();
-  } catch (error) {
-    logger.error(`findUsers error: ${error}`);
-    return Promise.reject(error);
-  }
-
-  try {
-    await Promise.all(
-      users.map(async user => {
-        const signedGames = user.signedGames.filter(signedGame => {
-          return signedGame.gameDetails;
-        });
-
-        const enteredGames = user.enteredGames.filter(enteredGame => {
-          return enteredGame.gameDetails;
-        });
-
-        const favoritedGames = user.favoritedGames.filter(favoritedGame => {
-          return favoritedGame;
-        });
-
-        if (
-          user.signedGames.length !== signedGames.length ||
-          user.enteredGames.length !== enteredGames.length ||
-          user.favoritedGames.length !== favoritedGames.length
-        ) {
-          await db.user.updateUser({
-            ...user,
-            signedGames,
-            enteredGames,
-            favoritedGames,
           });
         }
       })
--- src/test/scripts/removeInvalidGames.js
+++ src/test/scripts/removeInvalidGames.js
@@ -2,7 +2,7 @@
 import to from 'await-to-js';
 import { db } from 'db/mongodb';
 import { logger } from 'utils/logger';
-import { removeDeletedGamesFromUsers } from 'db/game/gameService';
+import { removeDeletedGamesFromUsers } from 'player-assignment/utils/removeDeletedGamesFromUsers';
 
 const removeInvalidGames = async (): Promise<any> => {
   let error;
@@ -10,7 +10,7 @@ const removeInvalidGames = async (): Promise<any> => {
   [error] = await to(db.connectToDb());
   if (error) return logger.error(error);
 
-  [error] = await to(removeDeletedGamesFromUsers);
+  [error] = await to(removeDeletedGamesFromUsers());
   if (error) return logger.error(`Error removing invalid games: ${error}`);
 
   [error] = await to(db.gracefulExit());
