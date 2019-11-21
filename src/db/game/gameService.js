dbd0022d84543f6d1aba7a46ba2817dfe38ee313
Arttu Hanska <arttu.hanska@gmail.com>
2019-11-21 21:01:52 +0200

    Move removeMovedGamesFromUsers to own file and add test

--- src/db/game/gameService.js
+++ src/db/game/gameService.js
@@ -1,10 +1,9 @@
 // @flow
 import _ from 'lodash';
-import moment from 'moment';
 import { logger } from 'utils/logger';
 import { GameModel } from 'db/game/gameSchema';
-import { db } from 'db/mongodb';
 import { removeInvalidSignupsFromUsers } from 'player-assignment/utils/removeInvalidSignupsFromUsers';
+import { removeMovedGamesFromUsers } from 'player-assignment/utils/removeMovedGamesFromUsers';
 import type { Game } from 'flow/game.flow';
 
 const removeGames = async (): Promise<void> => {
@@ -39,73 +38,6 @@ const removeDeletedGames = async (
     }
 
     await removeInvalidSignupsFromUsers();
-  }
-};
-
-const removeMovedGamesFromUsers = async (
-  updatedGames: $ReadOnlyArray<Game>
-): Promise<void> => {
-  logger.info('Remove moved games from users');
-  const currentGames = await findGames();
-
-  const movedGames = currentGames.filter(currentGame => {
-    return updatedGames.find(updatedGame => {
-      return (
-        currentGame.gameId === updatedGame.gameId &&
-        moment(currentGame.startTime).format() !==
-          moment(updatedGame.startTime).format()
-      );
-    });
-  });
-
-  if (!movedGames || movedGames.length === 0) return;
-
-  logger.info(`Found ${movedGames.length} moved games`);
-
-  let users = null;
-  try {
-    users = await db.user.findUsers();
-  } catch (error) {
-    logger.error(`findUsers error: ${error}`);
-    return Promise.reject(error);
-  }
-
-  try {
-    await Promise.all(
-      users.map(async user => {
-        const signedGames = user.signedGames.filter(signedGame => {
-          const movedFound = movedGames.find(movedGame => {
-            return movedGame.gameId === signedGame.gameDetails.gameId;
-          });
-          if (!movedFound) {
-            return signedGame;
-          }
-        });
-
-        const enteredGames = user.enteredGames.filter(enteredGame => {
-          const movedFound = movedGames.find(movedGame => {
-            return movedGame.gameId === enteredGame.gameDetails.gameId;
-          });
-          if (!movedFound) {
-            return enteredGame;
-          }
-        });
-
-        if (
-          user.signedGames.length !== signedGames.length ||
-          user.enteredGames.length !== enteredGames.length
-        ) {
-          await db.user.updateUser({
-            ...user,
-            signedGames,
-            enteredGames,
-          });
-        }
-      })
-    );
-  } catch (error) {
-    logger.error(`db.user.updateUser error: ${error}`);
-    throw new Error('No assign results');
   }
 };
 
