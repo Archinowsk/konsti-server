ca0c1b923ee34854e0e1ff63255bcb19b142391b
Arttu Hanska <arttu.hanska@gmail.com>
2020-01-18 17:18:02 +0200

    Update dependencies and fix flow errors

--- src/player-assignment/opa/utils/formatResults.js
+++ src/player-assignment/opa/utils/formatResults.js
@@ -2,11 +2,12 @@
 import _ from 'lodash';
 import type { OpaAssignResults } from 'flow/opaAssign.flow';
 import type { UserArray } from 'flow/user.flow';
+import type { Result } from 'flow/result.flow';
 
 export const formatResults = (
   assignResults: OpaAssignResults,
   playerGroups: $ReadOnlyArray<UserArray>
-) => {
+): $ReadOnlyArray<Result> => {
   const selectedPlayers = playerGroups
     .filter(playerGroup => {
       return assignResults.find(
@@ -16,16 +17,19 @@ export const formatResults = (
           assignResult.assignment !== -1
       );
     })
-    // $FlowFixMe: Cannot call `playerGroups.filter(...).flat` because property `flat` is missing in `Array` [1].
     .flat();
 
   return selectedPlayers.map(player => {
     return {
+      // $FlowFixMe: Cannot get `player.username` because property `username` is missing in mixed [1].
       username: player.username,
+      // $FlowFixMe: Cannot get `player.signedGames` because property `signedGames` is missing in mixed [1].
       enteredGame: player.signedGames.find(signedGame =>
         assignResults.find(
           assignResult =>
+            // $FlowFixMe: Cannot get `player.groupCode` because property `groupCode` is missing in mixed [1].
             (assignResult.id === player.groupCode ||
+              // $FlowFixMe: Cannot get `player.serial` because property `serial` is missing in mixed [1].
               assignResult.id === player.serial) &&
             assignResult.assignment === signedGame.gameDetails.gameId
         )
