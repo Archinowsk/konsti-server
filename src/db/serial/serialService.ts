c2f3e9d4f408299235563b1223f73bf0d406bd28
Altesmi <optikkanen@gmail.com>
2020-06-08 18:54:43 +0300

    ready for tests

--- src/db/serial/serialService.ts
+++ src/db/serial/serialService.ts
@@ -1,18 +1,20 @@
 import { logger } from 'utils/logger';
 import { SerialModel } from 'db/serial/serialSchema';
-import { SerialDoc } from 'typings/serial.typings';
+import { SerialDoc, Serial } from 'typings/serial.typings';
 
 const removeSerials = async () => {
   logger.info('MongoDB: remove ALL serials from db');
   return await SerialModel.deleteMany({});
 };
 
-const saveSerials = async (serials: readonly string[]): Promise<void> => {
+const saveSerials = async (
+  serials: readonly string[]
+): Promise<SerialDoc[]> => {
   const serialDocs = [] as SerialDoc[];
 
   for (const serial of serials) {
     if (await findSerial(serial)) {
-      logger.info(`Found serial ${serial} already in the database`);
+      logger.info(`Found serial ${serial} already from the database`);
       continue;
     }
     serialDocs.push(
@@ -22,11 +24,11 @@ const saveSerials = async (serials: readonly string[]): Promise<void> => {
     );
   }
 
-  let response;
+  let response: SerialDoc[];
   try {
     response = await SerialModel.create(serialDocs);
     logger.info(
-      `MongoDB: Serials data saved (${serialDocs.length} serials saved)`
+      `MongoDB: Serials data saved. (${serialDocs.length} serials saved)`
     );
     return response;
   } catch (error) {
@@ -35,7 +37,7 @@ const saveSerials = async (serials: readonly string[]): Promise<void> => {
   }
 };
 
-const findSerial = async (serial: string): Promise<any> => {
+const findSerial = async (serial: string): Promise<Serial | boolean> => {
   let response;
   try {
     response = await SerialModel.findOne({ serial }).lean();
--- src/utils/generateSerials.ts
+++ src/utils/generateSerials.ts
@@ -1,17 +1,19 @@
 import generator from 'generate-serial-number';
 import { logger } from 'utils/logger';
 import { db } from 'db/mongodb';
+import { SerialDoc } from '../typings/serial.typings';
 
 const isInt = (n) => parseInt(n, 10) % 1 === 0;
 
 const generateSerials = async (): Promise<void> => {
-  const serials: any = [];
-  const count = process.argv[2];
+  const serials: string[] = [];
+  const count: string = process.argv[2];
   if (!count || !isInt(count)) {
     logger.error('Give number parameter: "npm run generate-serials 10"');
   } else {
-    for (let i = 1; i <= parseInt(count, 10); i += 1) {
-      const serial = generator.generate(10);
+    const countNum: number = parseInt(count, 10);
+    for (let i = 1; i <= countNum; i += 1) {
+      const serial: string = generator.generate(10);
       serials.push(serial);
       logger.info(`${serial}`);
     }
@@ -23,13 +25,28 @@ const generateSerials = async (): Promise<void> => {
     }
 
     try {
-      await db.serial.removeSerials();
-    } catch (error) {
-      logger.error(`Error removing serials: ${error}`);
-    }
+      const savedSerials: SerialDoc[] = await db.serial.saveSerials(serials);
+      let residue: number = countNum - savedSerials.length;
+      if (residue !== 0) {
+        logger.info(
+          `${residue} new serials already in the database. Creating more serials`
+        );
+        while (residue !== 0) {
+          let residueSerials: string[] = [];
+          for (let i = 1; i <= residue; i += 1) {
+            const residueSerial = generator.generate(10);
+            residueSerials.push(residueSerial);
 
-    try {
-      await db.serial.saveSerials(serials);
+            logger.info(`new residue serial: ${residueSerial}`);
+          }
+
+          const residueSavedSerials = await db.serial.saveSerials(
+            residueSerials
+          );
+          residue = residue - residueSavedSerials.length;
+          residueSerials = [];
+        }
+      }
     } catch (error) {
       logger.error(`Error saving serials: ${error}`);
     }
