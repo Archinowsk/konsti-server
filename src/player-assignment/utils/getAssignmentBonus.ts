433b463b8bf3ebb78b26a126204e841b17ad4bc4
Arttu Hanska <arttu.hanska@gmail.com>
2020-09-17 17:03:19 +0300

    Rename variable

--- src/player-assignment/utils/getAssignmentBonus.ts
+++ src/player-assignment/utils/getAssignmentBonus.ts
@@ -2,17 +2,15 @@ import { config } from 'config';
 import { UserArray } from 'typings/user.typings';
 
 export const getAssignmentBonus = (playerGroup: UserArray): number => {
-  const numberOfGroupMemberEnterGameBefore = playerGroup.reduce((acc, curr) => {
+  const groupMembersWithEnteredGames = playerGroup.reduce((acc, curr) => {
     if (curr.enteredGames.length > 0) {
       return acc + 1;
     }
     return acc;
   }, 0);
 
-  const averageEnteredGames =
-    numberOfGroupMemberEnterGameBefore / playerGroup.length;
+  const averageEnteredGames = groupMembersWithEnteredGames / playerGroup.length;
 
   const bonus = averageEnteredGames < 0.5 ? config.firtSignupBonus : 0;
-
   return bonus;
 };
